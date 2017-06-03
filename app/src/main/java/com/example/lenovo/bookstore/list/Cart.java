5e909e1fd5520bfebeeed4c601e3e447b0ba70a2
Benyapa <ye.amity@gmail.com>
2017-06-04 04:22:42 +0700

    start adding cart

---
+++ app/src/main/java/com/example/lenovo/bookstore/list/Cart.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.bookstore.list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by lenovo on 6/4/2017.
+ */
+
+public class Cart extends AppCompatActivity {
+    private TextView totalPrice;
+
+}
